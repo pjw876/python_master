34c36972a29b2d6a9e74fa3676ca5485c72a7854
mars7421 <mars7421@naver.com>
2025-08-11 19:12:24 +0900

    [012] 배열의 평균값

--- [012] 배열의 평균값/배열의 평균값.py
+++ [012] 배열의 평균값/배열의 평균값.py
@@ -1,3 +1,10 @@
 def solution(numbers):
     answer = sum(numbers) / len(numbers)
     return answer
+
+# 다른 사람의 풀이
+# numpy를 사용하는 방법도 있다!
+# import numpy as np
+# def solution(numbers):
+#     # mean은 numpy에서 평균을 뜻함  
+#     return np.mean(numbers)
